901bf91c5d253a3458f92d1c5e07b94720f4e2eb
Ahmed Mujtba <ahmedmujtba@ahmeds-mbp.home>
2022-11-18 15:38:02 +0000

    testing deployment

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDatabase from "./config/MongoDB.js";
-import ImportData from "./DataImport.js";
-import productRouter from "./routes/Product.Routes.js";
-import { errorHandler, notFound } from "./middleware/Error.js";
-import userRouter from "./routes/User.Routes.js";
-import shoppingListRouter from "./routes/ShoppingList.Routes.js";
-import cors from "cors";
+// import ImportData from "./DataImport.js";
+// import productRouter from "./routes/Product.Routes.js";
+// import { errorHandler, notFound } from "./middleware/Error.js";
+// import userRouter from "./routes/User.Routes.js";
+// import shoppingListRouter from "./routes/ShoppingList.Routes.js";
+// import cors from "cors";
 
 dotenv.config();
 connectDatabase();
-const app = express();
-app.use(express.json());
-app.use(cors());
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
 
-//API
-app.use("/api/import", ImportData);
-app.use("/api/products", productRouter);
-app.use("/api/users", userRouter);
-app.use("/api/shopping-list/", shoppingListRouter);
+// //API
+// app.use("/api/import", ImportData);
+// app.use("/api/products", productRouter);
+// app.use("/api/users", userRouter);
+// app.use("/api/shopping-list/", shoppingListRouter);
 
-//ERROR HANDLERS
-app.use(notFound);
-app.use(errorHandler);
+// //ERROR HANDLERS
+// app.use(notFound);
+// app.use(errorHandler);
 
-app.get("/", (req, res) => {
-  res.send("API is live...");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is live...");
+// });
 
 const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, console.log(`server is running on port ${PORT}...`));
 
-export default app;
+// export default app;
